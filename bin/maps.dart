a6a92a5ebb299fa435bdeb823cea2242a84237b1
smmonir <asktomonir@gmail.com>
2024-03-15 23:57:14 +0600

    update_home

--- bin/maps.dart
+++ bin/maps.dart
@@ -1,8 +1,40 @@
-main(){
-  Map studentList={'Name':'Monir','Age':'24','Department': 'Computer Scienc ' };
+void main() {
 
-  print(studentList['Name']);
+  Map fruitList = {
+    'Name': 'Apple',
+    'Color': 'Red',
+    'Price': '\$2.5'
+  };
+  Map fruitList1 = {
+    'Name':'Banana',
+    'Color':'Yellow',
+    'Price': '\$1.0'
+  };
 
+
+  Map fruitList2 = {
+      'Name': 'Grapes',
+      'Color': 'Purple',
+      'Price': '\$3.0'
+    };
+
+
+  print(fruitList);
+  print(fruitList1);
+  print(fruitList2);
+
+}
+
+
+void displayFruitDetails () {
+
+
+
+
+
+}
+
+void applyPriceDiscount() {
 
 
 }
